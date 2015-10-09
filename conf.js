056dde45a639e74144f433862e4ab3dca2db1570
vincent_gor <1149536116@qq.com>
2015-10-09 17:33:34 +0000

    下载功能

--- conf.js
+++ conf.js
@@ -9,6 +9,8 @@ var conf = {
 start:   NODE_ENV=development PORT=80 forever -a -l /root/mywork/my_wechat/my_wechat.log start /root/mywork/my_wechat/bin/www
 restart: NODE_ENV=development PORT=80 forever -a -l /root/mywork/my_wechat/my_wechat.log restart /root/mywork/my_wechat/bin/www
 stop:    NODE_ENV=development PORT=80 forever -a -l /root/mywork/my_wechat/my_wechat.log stop /root/mywork/my_wechat/bin/www
+
+forever -a -l /home/ubuntu/workspace/update_app.log restart  /home/ubuntu/workspace/bin/www
 */
 
 module.exports = conf;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'hello, 粑粑' });
 });
 
+/* 最新的版本号 */
+router.get('/latest', function(req, res, next) {
+    var latest = '1.2.4';
+    var url = 'https://update-app-vincent-gor-1.c9.io/version/qq_1.2.4.apk';
+    console.log('查询最新版本');
+    res.json({ret_code: 0, version: latest, url: url});
+});
+
 module.exports = router;
